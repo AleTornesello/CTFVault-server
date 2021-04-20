82c7c7729da5b4ed7df4341962fb729d1c4f0350
Antonio Tornesello <alessandro.tornesello99@gmail.com>
2021-04-20 19:00:06 +0200

    feat: create class for application error management

---
+++ src/helpers/applicationError.js
@@ -0,0 +1,13 @@
+class ApplicationError extends Error {
+
+  constructor(message, status, code) {
+      super();
+      Error.captureStackTrace(this, this.constructor);
+      this.name = this.constructor.name;
+      this.message = message || '';
+      this.status = status || 500;
+      this.code = code || -1;
+  }
+}
+
+module.exports = ApplicationError;
